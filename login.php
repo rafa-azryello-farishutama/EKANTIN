8eb66bbad0f679ff78cc3d3804dea499667a1853
Muhammad Rafa Azryello Farishutama <rafaleon1601@gmail.com>
2026-02-25 09:29:52 +0700

    Login

--- login.php
+++ login.php
@@ -1,84 +1,96 @@
+<?php
+include 'database/koneksi.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $cek = $db_ekantin->query($sql);
+
+    if($cek->num_rows>0){
+        $data=$cek->fetch_assoc();
+
+        if(password_verify($password, $data['password'])){
+
+        $status = $data['status_aktif'];
+            if($status == "aktif"){
+                echo "<script>
+                    alert('Login Berhasil!');
+                    window.location='menu.php';
+                    </script>";
+                    exit;
+                } else {
+                 echo "<script>
+                    alert('Akun tidak aktif!');
+                    window.location='login.php';
+                    </script>";
+                    exit;
+                }
+
+        } else {
+            echo "<script>
+                    alert('Password salah!');
+                    window.location='login.php';
+                    </script>";
+                    exit;
+        }
+    } else {
+        echo "<script>
+                    alert('Username dan Password tidak ditemukan!');
+                    window.location='login.php';
+                    </script>";
+                    exit;
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style1.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login</title>
+<link rel="stylesheet" href="css/styleRegister.css">
 </head>
 <body>
 
-<?php
-$usernameError = false;
-$passwordError = false;
-$tulisanUsername = false;
-$tulisanPassword = false;
+<form method="POST">
+<div class="containerUtama">
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    <div class="kotakLogin2">
 
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    if ($username !== "rafa") {
-        $usernameError = true;
-        $tulisanUsername = true;
-    }
-
-    if ($password !== "1601") {
-        $passwordError = true;
-        $tulisanPassword = true;
-    }
-
-    if (!$usernameError && !$passwordError) {
-        echo "<script>alert('Login Berhasil!');</script>";
-    }
-}
-?>  
-    <form method="POST" action="">
-    <div class="kotakLogin">
-            <img class="logo" src="img/mayers.png">
-
-                <input type="text" class="kotakInput1 <?php if($usernameError) echo 'error'; ?>" name="username">
-
-                <div class="tulisan1 <?php if($tulisanUsername) echo 'error1'; ?>">
-                    <p>Username</p>
-                </div>
-            
-            <div class="kotak2">
-                <input type="password" class="kotakInput2 <?php if($passwordError) echo 'error'; ?>" name="password">
-    
-            <div class="tulisan2 <?php if($tulisanPassword) echo 'error1'; ?>">
-                <p>Password</p>
-            </div>
-            </div>
-
-            <div class="kotakbutton">
-                <button type="submit" class="tombolSubmit">Login</button>
+        <div class="header">
+            <img src="img/LOGO-EKANTIN.png" class="fotoLogo">
         </div>
 
-            <div class="jarakTulisan">
-                <div class="tulisanAkun">
-                    <a href="menu.php">Buat Akun?</a>
-                    <span>/</span>
-                    <a href="pesan.php">Lupa Password?</a>
-                </div>
+        <div class="jarakKotak2">
+
+            <div class="input-group">
+                <input type="text" class="kotakInput3" name="username" required>
+                <div class="tulisan2"><p>Username</p></div>
             </div>
+
+            <div class="input-group">
+                <input type="password" class="kotakInput4" name="password" required>
+                <div class="tulisan3"><p>Password</p></div>
+            </div>
+
+        </div>
+
+        <div class="kotakBawah">
+            <input type="submit" class="tombolSubmit" value="Submit">
+
+            <div class="kotakTulisan">
+                <a href="register.php">Kembali?</a>
+                <span>/</span>
+                <a href="menu.php">Lupa Password?</a>
+            </div>
+        </div>
+
     </div>
+
+</div>
 </form>
-
-    <script>
-    document.querySelectorAll("input").forEach(function(input) {
-        input.addEventListener("focus", function() {
-            this.classList.remove("error");
-            let targetTulisan = this.nextElementSibling;
-        
-            if (targetTulisan && (targetTulisan.classList.contains('tulisan1') || targetTulisan.classList.contains('tulisan2'))) {
-                targetTulisan.classList.remove("error1");
-            }
-        });
-    });
-</script>
-
-
-</body>
-</html>
