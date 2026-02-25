442a172f1e4afd1596b291161e2e868beead9fe8
Pirapieuy <nuralfanrafi@gmail.com>
2026-02-25 11:35:34 +0700

    beranda

---
+++ beranda.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styleberanda.css">
+</head>
+<body>
+<div class="container">
+        <div class="header"></div>
+        <div class="container2">
+            <div class="wadah1">
+                <img src="Ekantin.jpeg" alt="">
+            </div>
+            <div class="wadah2">
+            <div class="daftarKantin">
+                <div class="kantin">
+                    <a href="menu.php">
+                    <img src="kan1.webp" alt="">
+                    </a>
+                </div>
+                <div class="kantin">
+                    <img src="kan1.webp" alt="">
+                </div>
+                <div class="kantin">
+                    <img src="kan1.webp" alt="">
+                </div>
+                <div class="kantin">
+                    <img src="kan1.webp" alt="">
+                </div>
+                <div class="kantin">
+                    <img src="kan1.webp" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+     <div class="footer"></div>
+</div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylemenu.css">
+</head>
+<body>
+    <div class="container">
+            <div class="header">
+                <img src="Ekantin.jpeg" alt="">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="beranda.php">HOME</a>
+                            <a href="beranda.php">PESANAN</a>
+                            <a href="beranda.php">PEMBAYARAN</a>
+                            <a href="beranda.php">AKUN</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        <div class="container2">
+            <div class="samping">
+                <div class="satas">
+                    <img src="Ekantin.jpeg" alt="">
+                </div>
+                <div class="sbawah">
+                    <div class="isi"></div>
+                </div>
+            </div>
+            <div class="container3">
+                <div class="menu">
+                    <div class="wadah">
+                    <div class="box1">
+                        <img src="Ekantin.jpeg" alt="">
+                    </div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                    <div class="wadah">
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    <div class="box1"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="footer"></div>
+    </div>
+</body>
+</html>
